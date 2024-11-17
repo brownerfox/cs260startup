ade6611254db1bc49ac5ff5d60973e96de9ca6ed
brownerfox <taftjl123@gmail.com>
2024-11-16 19:56:34 -0700

    Added the endpoints for login and logout

--- service/index.js
+++ service/index.js
@@ -20,3 +20,23 @@ apiRouter.post('/auth/create', async (req, res) => {
       res.send({ token: user.token });
     }
   });
+
+  apiRouter.post('/auth/login', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      if (req.body.password === user.password) {
+        user.token = uuid.v4();
+        res.send({ token: user.token });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+
+  apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+      delete user.token;
+    }
+    res.status(204).end();
+  });
