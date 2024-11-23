386a2d20ee82ab195a6f16f82036c7d9544e5087
brownerfox <taftjl123@gmail.com>
2024-11-23 14:13:14 -0700

    Changed some of the formatting for feed.jsx

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -14,27 +14,27 @@ export function Feed({ posts = [], userName }) {
       });
   }, []);
 
-    console.log(posts);
-    return (
-      <div className="feed">
-        {posts.length > 0 ? (
-          posts.map((post, index) => (
-            <Post
-              key={index}
-              location={post.location}
-              rodType={post.rodType}
-              baitType={post.baitType}
-              imageUrl={post.imageUrl}
-              caption={post.caption}
-              time={post.time}
-              userName={userName}
-            />
-          ))
-        ) : (
-          <p>No posts to display</p>
-        )}
-      </div>
-    );
-  }
+  console.log(posts);
+  return (
+    <div className="feed">
+      {posts.length > 0 ? (
+        posts.map((post, index) => (
+          <Post
+            key={index}
+            location={post.location}
+            rodType={post.rodType}
+            baitType={post.baitType}
+            imageUrl={post.imageUrl}
+            caption={post.caption}
+            time={post.time}
+            userName={userName}
+          />
+        ))
+      ) : (
+        <p>No posts to display</p>
+      )}
+    </div>
+  );
+}
 
 export default Feed;
